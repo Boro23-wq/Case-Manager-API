852f4db9f6ca1a6bf50839e71157d3733059f1e5
Sintu Boro <sintuboro@Sintus-MBP.lan>
2022-11-02 17:57:54 -0500

    added crud endpoints for cases

---
+++ src/cases/entities/case.entity.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { PatientCase } from '@prisma/client';
+
+export class CaseEntity implements PatientCase {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  modifiedAt: Date;
+
+  @ApiProperty()
+  status: string;
+
+  @ApiProperty()
+  subject: string;
+
+  @ApiProperty()
+  caseManagerEmail: string;
+
+  @ApiProperty()
+  categoryTitle: string;
+
+  @ApiProperty()
+  solutionId: number;
+
+  @ApiProperty()
+  patientEmail: string;
+
+  @ApiProperty()
+  severityLevel: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  doctorEmail: string | null;
+}
